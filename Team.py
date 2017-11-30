57730264b5add369549407088d76d09813895520
Brian Ge <briange1997@gmail.com>
2017-11-29 23:23:43 -0500

    Commented

--- Team.py
+++ Team.py
@@ -4,12 +4,14 @@ class Team():
 		self.snakes = []
 		self.color = color
 
+	# Return current score of team, which is equal to the sum of snake lengths
 	def getScore(self):
 		score = 0
 		for snake in self.snakes:
 			score += snake.length
 		return score
 
+	# Update a member of the team
 	def updateSnake(self, snake_id, index):
 		self.snakes[snake_id].updateSnake(index)
 
--- pygame_gui.py
+++ pygame_gui.py
@@ -20,21 +20,16 @@ class Game():
         self.width = width * self.pixel_size
         self.height = height * self.pixel_size
 
+        # Hard coded color scheme for now
         team_colors = [(0, 255, 0), (66,238,244)]
 
         # initialize Game State for first time
         self.state = GameState(2, team_colors, width, height)
 
-        # print "original state: "
-        # for snake in self.state.snakes:
-        #     print snake.position
-        # for snake in self.state.snakes2:
-        #     print snake.position
-
         # create some random snakes
         self.state.addRandoSnake(width, height, 10, 0)
         self.state.addRandoSnake(width, height, 5, 0)
-        self.state.addRandoSnake(width, height, 8, 0)
+        self.state.addRandoSnake(width, height, 8, 1)
         
         # Create the screen with black background
         self.screen = pygame.display.set_mode((self.width, self.height))
@@ -51,17 +46,20 @@ class Game():
         self.screen.fill(color, (xcor * self.pixel_size, ycor * self.pixel_size, self.pixel_size, self.pixel_size))
         return
 
+    # Draw an apple on the screen
     def drawApple(self, xcor, ycor):
         apple = pygame.image.load("images/apple.png")
         apple = pygame.transform.scale(apple, (self.pixel_size, self.pixel_size))
         self.screen.blit(apple, (xcor * self.pixel_size, ycor * self.pixel_size, self.pixel_size, self.pixel_size))
 
+    # Draw eyes on snake at correct pixel with correct orientation
     def drawEyes(self, x, y, direction):
         eyes = pygame.image.load("images/eyes2.png")
         eyes = pygame.transform.scale(eyes, (self.pixel_size, self.pixel_size))
         eyes = pygame.transform.rotate(eyes, self.directionToAngle(direction))
         self.screen.blit(eyes, (x * self.pixel_size, y * self.pixel_size, self.pixel_size, self.pixel_size))
 
+    # Return the angle that the eyes need to be rotated by based on direction
     def directionToAngle(self, direction):
         if direction == "north":
             return 180
@@ -72,14 +70,14 @@ class Game():
         else:
             return 0
 
+    # Update the game screen based on the current game state
     def updateDisplay(self):
         state = self.state
 
+        # Redraw the black background
         self.screen.fill((0, 0, 0))
 
-        # Placeholder to iterate through snake coordinates and fill pixels with white
-        # print state.snakes[0].position
-
+        # Iterate through every snake and draw all pixels and eyes
         for team in state.teams:
             for snake in team.snakes:
                 for x, y in snake.position:
@@ -90,8 +88,8 @@ class Game():
                     else: 
                         self.drawPixel(x, y, team.color)
 
+        # Iterate through food and draw all apples
         for x, y in state.food:
-            # self.drawPixel(x, y, (255, 0, 0))
             self.drawApple(x, y)
 
         # Update screen
@@ -110,17 +108,22 @@ class Game():
                 if event.type == pygame.QUIT:
                     raise SystemExit
 
+            # Update the game screen
             self.updateDisplay()
+
+            # Iterate through each snake and tell it to move
             for team in self.state.teams:
                 for snake in team.snakes:
                     if snake.isAlive():
                         # snake.move(random.choice(snake.getActions()))
                         snake.move("forward")
 
+            # Update the game state based on snake movements (check collisions)
             self.state.update()
+
+            # Update each teams' score
             for team in self.state.teams:
                 print team.getScore()
-            
 
             # delay between timesteps
             sleep(0.2)
